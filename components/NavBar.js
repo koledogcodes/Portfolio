7e4c9e16703259d005b31b7d1a682b0d0e49efbb
Kolade <kololo@Kolades-MBP.modem>
2023-11-05 14:16:54 +1100

    ENHANCE: Navbar much more responsive on mobile

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import NavBarItem from "./NavBarItem";
 
 export default function NavBar() {
-    return (
-        <nav className="flex w-screen bg-white mb-14">
-            <NavBarItem heading>Kolade.dev</NavBarItem>
+    const [expanded, setExpanded] = useState(false);
 
-            <nav className="flex items-center w-full justify-end">
+    return (
+        <nav className="flex flex-col w-screen bg-white mb-14 sm:flex-row">
+            <NavBarItem heading>
+                <span className="flex">
+                    <div className="flex-none">Kolade.dev</div>
+                    <div className="flex items-center flex-row w-full justify-end sm:hidden" onClick={() => setExpanded(expanded => !expanded)}>
+                        ...
+                    </div>
+                </span>
+            </NavBarItem>
+
+
+            <div data-expand={expanded} className="flex flex-col items-center w-full justify-end sm:flex-row max-md:data-[expand=false]:hidden">
                 <NavBarItem>Home</NavBarItem>
                 <NavBarItem>About</NavBarItem>
                 <NavBarItem>Projects</NavBarItem>
                 <NavBarItem>Contact</NavBarItem>
-            </nav>
+            </div>
         </nav>
     )
 }
--- components/NavBarItem.js
+++ components/NavBarItem.js
@@ -7,6 +7,6 @@ export default function NavBarItem({ children, heading = false }) {
     }
 
     return (
-        <div className="flex-none w-auto px-3 py-4 cursor-pointer font-semibold hover:text-red-600 hover:bg-slate-100">{children}</div>
+        <div className="flex-none w-full sm:w-auto px-3 py-4 cursor-pointer font-semibold hover:text-red-600 hover:bg-slate-100">{children}</div>
     )
 }
