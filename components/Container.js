d0c174b64fa3e43e280565d62cc52ba206ff4a46
Kolade <kololo@Kolades-MacBook-Pro.local>
2023-12-02 17:45:27 +1100

    NEW: Added some desc for homepage

--- components/Container.js
+++ components/Container.js
@@ -1,8 +1,8 @@
 
-export default function Container({ children }) {
+export default function Container({ children, simple }) {
     return (
         <div className="flex justify-center">
-            <div className="w-2/3 h-full bg-red-200">{children}</div>
+            <div className="w-2/3 h-full">{children}</div>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Container from "../../components/Container";
 import NavBar from "../../components/NavBar";
 
@@ -7,7 +8,21 @@ export default function Home() {
       <NavBar />
 
       <Container>
-        <h1 className="text-6xl">Software Engineer</h1>
+        <div className="flex flex-col-reverse md:flex-row h-auto">
+
+          <section className="flex flex-col basis-1/2">
+            <h1 className="basis-1/2 text-6xl">Software Engineer</h1>
+            <p className=" basis-1/2 text-lg">Hi, I'm Kolade Olorunnife. A passionate Software Engineer with experience in web and mobile development based in Melbourne, Australia. 📍</p>
+          </section>
+
+          <section className="basis-1/2">
+            <div className="flex flex-row-reverse">
+              <Image className="mx-auto rounded-full" src={"/profile.jpeg"} alt={""} width={128} height={128} />
+            </div>
+          </section>
+
+        </div>
+
       </Container>
     </>
   )
