a934ed9cc14273107629f1b2dc89fc57562b8e43
electrotech14@hotmail.com <Lekolade1213>
2023-12-04 00:54:27 +1100

    NEW: Added favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,12 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-
+// 🖥️
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon-96.png" />
+        <title>Kolade Olorunnife | Software Engineer</title>
+      </Head>
       <body className='bg-slate-100'>
         <Main />
         <NextScript />
