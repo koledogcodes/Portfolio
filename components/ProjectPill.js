5226138ca6ad17b096f87118c356930426573b16
electrotech14@hotmail.com <Lekolade1213>
2023-12-03 19:09:43 +1100

    ENHANCE: Added animations for social icons and self portrait

---
+++ components/ProjectPill.js
@@ -0,0 +1,6 @@
+
+export default function ProjectPill() {
+    return (
+        <div>ProjectPill</div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <NavBar />
 
       <Container>
-        <div className="flex flex-col-reverse md:flex-row">
+        <div id="home" className="flex flex-col-reverse md:flex-row">
 
           <section className="flex flex-col basis-1/2 my-auto">
             <h1 className="basis-1/2 text-6xl py-5 text-center md:text-start">Software Engineer</h1>
@@ -17,15 +17,15 @@ export default function Home() {
             {/* Socials */}
             <section className="flex justify-center md:justify-start">
               <a href="https://www.linkedin.com/in/stephen-olorunnife-84b449177/" target="_blank">
-                <InlineIcon className="text-4xl text-blue-700" icon="mdi:linkedin" />
+                <InlineIcon className="text-4xl text-blue-700 icon" icon="mdi:linkedin" />
               </a>
 
               <a href="https://gitlab.com/KoolaidDaBeast" className="px-2 my-auto" target="_blank">
-                <InlineIcon className="text-4xl" icon="mdi:github" />
+                <InlineIcon className="text-4xl icon" icon="mdi:github" />
               </a>
 
               <a href="https://twitter.com/KoolaidDaBeast" className="my-auto" target="_blank">
-                <InlineIcon className="text-4xl text-blue-400" icon="mdi:twitter" />
+                <InlineIcon className="text-4xl text-blue-400 icon" icon="mdi:twitter" />
               </a>
             </section>
           </section>
@@ -33,7 +33,9 @@ export default function Home() {
           {/* Self Portait */}
           <section className="basis-1/2 h-[320px]">
             <div className="flex flex-row-reverse relative h-full">
-              <img className="mx-auto rounded-3xl" src={"/selfie.jpg"} alt={"/selfie.jpeg"} />
+              <div className="flex p-[4px] profile-img bg-black">
+                <img className="mx-auto rounded-3xl profile-img" src={"/selfie.jpg"} alt={"/selfie.jpeg"} />
+              </div>
             </div>
           </section>
 
