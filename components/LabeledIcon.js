ef5ae54abd1a9b4915728685457600de761b6507
Kolade Olorunnife <electrotech14@hotmail.com>
2023-12-04 23:17:54 +1100

    NEW: Added button to download my resume

--- components/LabeledIcon.js
+++ components/LabeledIcon.js
@@ -2,7 +2,7 @@ import { InlineIcon } from "@iconify/react";
 
 export default function LabeledIcon({ icon, title, subTitle, padded }) {
     return (
-        <div className={"flex flex-row p-0 md:data-[lp=true]:pl-[10%]"} data-lp={padded != undefined}>
+        <div className={"basis-1/4 flex flex-row p-0"} data-lp={padded != undefined}>
             <InlineIcon icon={icon} className="shadow-lg hover:shadow-none md:hover:shadow-lg border-[1px] border-slate-100 p-2 rounded-full text-6xl icon text-red-400" />
 
             <div className="flex flex-col flex-wrap my-auto pl-2">
--- sections/ContactUs.js
+++ sections/ContactUs.js
@@ -9,16 +9,20 @@ export default function ContactUs() {
                 <p className="text-black text-2xl font-bold pb-3">Don&apos;t be shy! Hit me up below! 👇</p>
             </section>
 
-            <section className="flex flex-col md:flex-row pt-8">
+            <section className="flex flex-col xl:flex-row place-content-evenly pt-8">
                 <LabeledIcon icon="material-symbols:map" title="Location" subTitle="Melbourne, Australia" />
 
                 <span className="py-2 md:py-0" />
 
-                <LabeledIcon icon="material-symbols:mail-outline" title="Mail" subTitle={<a className="break-all" href="mailto:electrotech14@hotmail.com">electrotech14@hotmail.com</a>} padded />
+                <LabeledIcon icon="mdi:paper" title="Resume" subTitle={<a className="break-all" target="_blank" href="./resume_2023.pdf">Download Resume</a>} padded />
 
                 <span className="py-2 md:py-0" />
 
                 <LabeledIcon icon="mdi:phone" title="Call" subTitle={<a className="break-all" href="tel:+61457289885">+61 457 289 885</a>} padded />
+
+                <span className="py-2 md:py-0" />
+
+                <LabeledIcon icon="material-symbols:mail-outline" title="Mail" subTitle={<a className="break-all" href="mailto:electrotech14@hotmail.com">electrotech14@hotmail.com</a>} padded />
 
             </section>
         </Container>
