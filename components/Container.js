55479c232f0e1b5cc4700ae479ae9801e509a90b
Kolade Olorunnife <electrotech14@hotmail.com>
2023-12-04 23:20:56 +1100

    ENHANCE: Very minor ui tweaks

--- components/Container.js
+++ components/Container.js
@@ -2,14 +2,14 @@
 export default function Container({ children, light, dark, id }) {
     if (light != undefined) {
         return (
-            <div id={id} className="flex justify-center bg-white py-[5%]">
+            <div id={id} className="flex justify-center bg-white py-[6%]">
                 <div className="w-3/5 h-full">{children}</div>
             </div>
         )
     }
 
     return (
-        <div id={id} className="flex justify-center py-[5%]">
+        <div id={id} className="flex justify-center py-[6%]">
             <div className="w-3/5 h-full">{children}</div>
         </div>
     )
